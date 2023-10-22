50a2aa5b7d5b7275dba86b8b79f56d5244b3889b
S.Go <swastikgorai@gmail.com>
2023-10-22 21:38:25 +0530

    update models and schemas

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,60 +1,147 @@
 from pydantic import BaseModel
-from typing import Optional, List
-from datetime import datetime
-######################################################
-################### Users ############################
-######################################################
+from typing import Optional
+
+#######################################################
+################# UserAuth ###########################
+#######################################################
+
+class UserForAuthBase(BaseModel):
+    name: str
+    email: str
+    password: str
+    is_disabled: bool
+
+class UserForAuthUpdate(UserForAuthBase):
+    pass
 
 
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    
-class TokenData(BaseModel):
-    username: Optional[str] = None
-    
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-    
-class UserPasswd(User):
-    hashed_password: str
-    
-class UserDetails(User):
+#######################################################
+################# User ###############################
+#######################################################
+class UserBase(BaseModel):
+    user_id: int
     phone: str
-    profile_pic: Optional[str] = None
-    birth_date: Optional[datetime] = None
-    gender: Optional[str] = None
-    bio: Optional[str] = None
-    city: Optional[str] = None
-    state: Optional[str] = None
-    country: Optional[str] = None
-    twitter_id: Optional[str] = None
-    instagaram_id: Optional[str] = "NA"
-    followers: Optional[int] = None
-    following: Optional[int] = None
-    admin_status: Optional[bool] = False
-    admin_status_val: Optional[str] = "NA"
-    
+    profile_pic: Optional[str]
+    birth_date: str
+    gender: str
+    bio: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    country: Optional[str]
+    twitter_id: Optional[str]
+    instagaram_id: str
+    followers: int
+    following: int
+    admin_status: bool
+    admin_status_val: str
+
+class UserUpdate(UserBase):
+    pass
 
 
-######################################################
-################### Content ##########################
-######################################################
-
-class Content(BaseModel):
+#######################################################
+################# Content ############################
+#######################################################
+class ContentBase(BaseModel):
     content: str
     category: int
     sub_category: int
     user: int
-    created_at: datetime
-    updated_at: datetime
-    likes: int    
-    citation: Optional[str] = None
-    is_public: Optional[bool] = False
-    
-    class Config:
-        orm_mode = True
-    
+    citation: Optional[str]
+    is_public: bool
+
+class ContentUpdate(ContentBase):
+    pass
+
+
+#######################################################
+################# Comments ############################
+#######################################################
+
+class CommentsBase(BaseModel):
+    comment_content: str
+    comment_likes: int
+    comment_user: int
+    flag_is_updated: bool
+    flag_show: bool
+    flag_comment_of_comment: bool
+
+class CommentsUpdate(CommentsBase):
+    pass
+
+
+#######################################################
+################# Content_comment #####################
+#######################################################
+
+class ContentCommentBase(BaseModel):
+    content_id: int
+    comment_id: int
+
+class ContentCommentUpdate(ContentCommentBase):
+    pass
+
+
+#######################################################
+################# Comment_comment #####################
+#######################################################
+
+class CommentCommentBase(BaseModel):
+    head_comment_id: int
+    reply_comment_id: int
+
+class CommentCommentUpdate(CommentCommentBase):
+    pass
+
+
+#######################################################
+################# Category ############################
+
+
+class CategoryBase(BaseModel):
+    category_name: str
+    category_description: str
+    category_pic: Optional[str]
+
+class CategoryUpdate(CategoryBase):
+    pass
+
+
+#######################################################
+################# Sub_Category ########################
+#######################################################
+
+class SubCategoryBase(BaseModel):
+    sub_category_name: str
+    sub_category_description: str
+    sub_category_pic: Optional[str]
+    category_id: int
+
+class SubCategoryUpdate(SubCategoryBase):
+    pass
+
+
+#######################################################
+################# Collection #########################
+#######################################################
+
+class CollectionBase(BaseModel):
+    collection_name: str
+    collection_description: str
+    collection_user: int
+    collection_posts: int
+
+class CollectionUpdate(CollectionBase):
+    pass
+
+
+#######################################################
+################# Content_collection ##################
+#######################################################
+
+class BookmarkBase(BaseModel):
+    bookmark_user: int
+    bookmark_content: int
+
+class BookmarkUpdate(BookmarkBase):
+    pass
--- src/models/models.py
+++ src/models/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Time, Boolean, DateTime, Text
 from uuid import uuid4
 import datetime
+from sqlalchemy.orm import relationship
+
 
 class User_forAuth:
     __tablename__ = 'user'
@@ -32,6 +34,9 @@ class User:
     following = Column(Integer, ForeignKey("user.id"))
     admin_status = Column(Boolean, default=False)
     admin_status_val = Column(String(100), default="NA")
+    
+    contents = relationship("Content", back_populates="user")
+    bookmarks = relationship("Bookmark", back_populates="bookmark_user")
 
 
 class Content:
@@ -43,11 +48,15 @@ class Content:
     sub_category = Column(Integer, ForeignKey("sub_category.id"))
     user = Column(Integer, ForeignKey("user.id")) 
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    last_updated_at = Column(DateTime, default=datetime.datetime.utcnow)
     likes = Column(Integer, default=0)
     # comments = Column(Text) ## make so as to store Foriegn key for multiple comments
     citation = Column(Text)
     is_public = Column(Boolean, default=False)
+    head_comments = Column(Integer, ForeignKey("comments.id"))
+    user = relationship("User", back_populates="contents")
+    
+    comments = relationship("Comments", back_populates="content")
     
 class Comments:
     __tablename__ = 'comments'
@@ -64,9 +73,12 @@ class Comments:
     
 class Content_comment:
     __tablename__ = "content_comment"
+    
     id = Column(Integer, primary_key = True, index = True)
     content_id = Column(Integer, ForeignKey("content.id"))
     comment_id = Column(Integer, ForeignKey("comments.id"))
+    
+    content = relationship("Content", back_populates="comments")
     
 class Comment_comment:
     __tablename__ = "comments_comment"
@@ -92,6 +104,8 @@ class Sub_Category:
     sub_category_pic = Column(String(100)) ## store in s3
     category_id = Column(Integer, ForeignKey("category.id"))
     
+    
+    
 class Collection:
     __tablename__ = 'collection'
     
@@ -101,4 +115,11 @@ class Collection:
     colection_user = Column(Integer, ForeignKey("user.id"))
     collection_posts = Column(Integer, ForeignKey("content.id"))
     
+class Bookmark:
+    __tablename__ = 'bookmark'
     
+    id = Column(Integer, primary_key=True, index=True)
+    bookmark_user = Column(Integer, ForeignKey("user.id"))
+    bookmark_content = Column(Integer, ForeignKey("content.id"))
+    
+    user = relationship("User", back_populates="bookmarks")
